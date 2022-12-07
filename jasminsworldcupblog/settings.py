20679fdb285dd14af11379eb86d33c1aa8f90aa9
Jasmin Juzbasic <jasmin.juzbasic@gmail.com>
2022-12-07 17:50:03 +0100

    create admin, configure summernote and create test post

--- jasminsworldcupblog/settings.py
+++ jasminsworldcupblog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jasmins-world-cup-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['jasmins-world-cup-blog.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'jwcblog',
 ]
 
@@ -131,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudniarty_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
